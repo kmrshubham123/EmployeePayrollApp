a8f82b8de0bb5399ee07bd536ad34a7fb7aa67bf
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-19 16:57:52 +0530

    [Kumar Shubham]Added:validate the Employee Name

--- javaScript/payrollform.js
+++ javaScript/payrollform.js
@@ -5,3 +5,13 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
+//UC9-validate name
+const text = document.querySelector('#name');
+const textError = document.querySelector('#text-error');
+text.addEventListener('input', function () {
+    let namRegex = new RegExp('^[A-Z][a-zA-z\\s]{2,}$');
+    if (namRegex.test(text.value))
+        textError.textContent = "";
+    else
+        textError.textContent = "Incorrect First letter should be captial";
+});
